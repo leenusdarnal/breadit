c6690c21668adeda3c3c75ed033326d6b146f2ec
leenusdarnal <leenus.darnal@gmail.com>
2023-10-05 00:39:11 +0530

    sub details page and pkg updates

--- src/app/api/subreddit/route.ts
+++ src/app/api/subreddit/route.ts
@@ -6,31 +6,38 @@ import { z } from 'zod'
 export async function POST(req: Request) {
   try {
     const session = await getAuthSession()
+
     if (!session?.user) {
       return new Response('Unauthorized', { status: 401 })
     }
+
     const body = await req.json()
     const { name } = SubRedditValidator.parse(body)
+
     const subredditExits = await db.subreddit.findFirst({
       where: {
         name,
       },
     })
+
     if (subredditExits) {
       return new Response('Subreddit already exits', { status: 409 })
     }
+
     const subreddit = await db.subreddit.create({
       data: {
         name,
         creatorId: session.user.id,
       },
     })
+
     await db.subscription.create({
       data: {
         userId: session.user.id,
         subredditId: subreddit.id,
       },
     })
+
     return new Response(subreddit.name)
   } catch (error) {
     if (error instanceof z.ZodError) {
